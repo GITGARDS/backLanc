ef96b70f1b2455778aa15821a161ed873532582d
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2025-09-21 15:08:11 -0300

    first commit

---
+++ src/main/java/app/BackLancApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLancApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLancApplication.class, args);
+	}
+
+}
